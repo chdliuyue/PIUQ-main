70e965a36ca219fb7cfa2ccf1fe8f90775af3d1f
Yue Liu <150256922+chdliuyue@users.noreply.github.com>
2025-11-21 20:01:41 +0800

    Improve centerline construction from lane markings

--- tests/test_highd_dataset.py
+++ tests/test_highd_dataset.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pytest
+import numpy as np
 
 from piuq.data.datasets.highd import HighDDataset
 
@@ -59,3 +60,34 @@ def test_tracks_meta_validation_errors():
 
     with pytest.raises(ValueError, match="numFrames"):
         dataset._standardize_tracks_meta(incomplete_meta)
+
+
+def test_lane_center_frenet_offsets_small():
+    dataset = HighDDataset(centerline_points=20)
+    lane_markings = [
+        np.array([[0.0, 0.0], [50.0, 1.0], [100.0, 2.0]]),
+        np.array([[0.0, 4.0], [50.0, 5.0], [100.0, 6.0]]),
+    ]
+
+    dataset.recording_context[1] = {
+        "lane_markings": lane_markings,
+        "driving_direction": 1.0,
+    }
+
+    sample_x = np.linspace(0.0, 100.0, num=30)
+    lower_y = np.interp(sample_x, lane_markings[0][:, 0], lane_markings[0][:, 1])
+    upper_y = np.interp(sample_x, lane_markings[1][:, 0], lane_markings[1][:, 1])
+    center_y = 0.5 * (lower_y + upper_y)
+
+    df = pd.DataFrame(
+        {
+            "recording_id": 1,
+            "track_id": 1,
+            "frame": np.arange(len(sample_x)),
+            "x": sample_x,
+            "y": center_y,
+        }
+    )
+
+    frenet_df = dataset.to_frenet(df)
+    assert np.max(np.abs(frenet_df["n"])) < 1e-6
